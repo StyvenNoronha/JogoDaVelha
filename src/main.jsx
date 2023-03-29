f8a0b56493f5aa00195fade0baee26dedab66839
Styven Noronha <styvenn16@gmail.com>
2023-03-29 14:25:58 -0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Hello from './components/Hello/Hello'
+
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Hello />
+    <Hello />
+  </React.StrictMode>
+)
