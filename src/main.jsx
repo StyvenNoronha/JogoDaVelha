886c1d09f80ae6a68d43ce69be7686375ae4ca2b
Styven Noronha <styvenn16@gmail.com>
2023-03-31 06:54:57 -0300

    Começamos a nossa arquitetura de pastas

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './style/generic/reset.css'
+import './style/elements/base.css'
+import './style/settings/colors.css'
 import Hello from './components/Hello/Hello'
 import Nav from './components/Nav/Nav'
 
