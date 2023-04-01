d26592ba2047b1eba42d0752cf1b72af03e673f5
Styven Noronha <styvenn16@gmail.com>
2023-04-01 06:35:48 -0300

    Criou o Logo

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,16 @@ import './style/generic/reset.css'
 import './style/elements/base.css'
 import './style/settings/colors.css'
 import Sobre from './objects/Sobre'
+import Logo from './objects/Logo'
 
 
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-  <Sobre/>
+    <>
+      <Logo/>
+      <Sobre/>
+    </>
   </React.StrictMode>
 )
---
+++ src/objects/Logo/index.jsx
@@ -0,0 +1,6 @@
+import React from "react"
+import LogoStyven from "../../img/STYVEN.png"
+
+const Logo = () => <img src={LogoStyven} alt="Logo" />
+
+export default Logo
