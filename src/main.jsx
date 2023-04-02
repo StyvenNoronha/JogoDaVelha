aa785a626448372dd9ca167e8e2a8ad1f1ab8b43
Styven Noronha <styvenn16@gmail.com>
2023-04-02 09:01:28 -0300

    Criado o Object Cartao com seu css

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client'
 import './style/generic/reset.css'
 import './style/elements/base.css'
 import './style/settings/colors.css'
+import './style/settings/spacing.css'
 import Header from './components/Header'
+import Cartao from './objects/Cartao'
 
 
 
@@ -14,6 +16,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     
     <Header/> 
-    
+    <Cartao/>
   </React.StrictMode>
 )
